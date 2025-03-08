d591cff069ee4728f64d0046577c45af5f2b7a5b
sritejathuraka <thuraka.sriteja@gmail.com>
2025-03-07 22:20:20 -0500

    Added:(day_6_operators)

---
+++ day_6_bitwise_operators.py
@@ -0,0 +1,37 @@
+##Bitwise Operators
+# 
+# #AND (&)
+a = 5
+b = 6
+print("result for a & b is",a & b )
+
+print(bin(a & b))
+
+##OR(|)
+a = 10
+b = 11
+
+print("Result for OR |", a | b)
+
+
+# XOR (^)
+
+a = 14
+b = 15
+print("result for XOR is", a ^ b)
+
+
+# NOT (~)
+
+a = 12
+print("result for NOT", ~a)
+
+
+#Left Shift <<
+a = 9
+print("Left shift by 1", a << 2)
+
+#RIGHT Shift <<
+a = 9
+print("RIGHT  shift by 1", a >> 2)
+
