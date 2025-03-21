253d6ee0de948fe217344af49fcaf8dbc302d1f4
sritejathuraka <thuraka.sriteja@gmail.com>
2025-03-20 21:38:52 -0400

    Added(day_11_loops)

---
+++ day_11_loops.py
@@ -0,0 +1,18 @@
+##** Loops in python **
+## For loop
+nameList = ["Arjun", "Nano", "Sunny", "Ramu"]
+for i in nameList:
+ print(i)
+
+
+ ## While loop
+i = 0 #initialize i
+while i <= 10:
+    print(i)
+    i += 1
+    
+
+
+
+
+   
