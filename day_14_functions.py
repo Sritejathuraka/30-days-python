3de751e65b93997d9419b5a86624a6e9f45353cf
sritejathuraka <thuraka.sriteja@gmail.com>
2025-04-09 11:41:28 -0400

    functions

---
+++ day_14_functions.py
@@ -0,0 +1,11 @@
+## function implementation in python
+
+def coffee_machine():
+    print("Grinding coffee beans...")
+    print("Pouring hot water...")
+    print("Adding milk...")
+    print("Adding sugar...")
+    print("Your coffee is ready!")
+
+coffee_machine()
+
---
+++ day_15_functions_arguments_returns.py
@@ -0,0 +1,6 @@
+def addition(a, b):
+   return  a + b
+
+total_addition = addition(3, 8)
+
+print('Total Addition is ', total_addition)
